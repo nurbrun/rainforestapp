197cd009cc720285452c0d9e3b4764cc8ad5bcba
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-11-10 17:53:18 -0500

    Seeds from CSV

--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+list = [
+	{
+		name: "Boots",
+		description: "Were made for walkin'",
+		price_in_cents: 20000
+	},
+	{
+		name: "Shoes",
+		description: "Just do it.",
+		price_in_cents: 20000
+	},
+	{
+		name: "Goat",
+		description: "Make your own cheese.",
+		price_in_cents: 20000
+	},
+	{
+		name: "Hairspray",
+		description: "For ultimate lift.",
+		price_in_cents: 20000
+	}
+]
+
+csv = CSV.read(Rails.root + "db/data.csv")
+csv[1..-1].each do |entry|
+	Product.create(
+		name: entry[0],
+		description: entry[1],
+		price_in_cents: entry[2]
+	)
+end
+
+# binding.pry
+# list.each do |item|
+# 	Product.create(
+# 		name: item[:name], 
+# 		description: item[:description], 
+# 		price_in_cents: item[:price_in_cents]
+# 	)
+# end
