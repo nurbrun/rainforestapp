2bd0a0e40999ae1f1d31e0df839f2e6fdf743c4e
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-11-11 17:25:07 -0500

    Adding logic to display 'Signed in as' or 'Login or Sign up' in the application view

--- rainforest/app/controllers/sessions_controller.rb
+++ rainforest/app/controllers/sessions_controller.rb
@@ -3,18 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:email])
+  	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
   		redirect_to products_url, notice: "Logged in!"
   	else
+  		flash.now[:alert] = "Invalid email or password"
   		render "new"
   	end
 
   end
 
   def destroy
-  	sessio[:user_id]=nil
+  	session[:user_id]=nil
   	redirect_to products_url, notice: "Logged out!"
   end
 end
